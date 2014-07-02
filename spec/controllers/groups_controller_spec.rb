9ee12be1d8a80f832c85ce2bccb9f10bb2fd0d49
Sammy <flouts@gmail.com>
2014-07-02 13:35:57 +0200

    implemented non authenticated access behaviour

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -31,18 +31,26 @@ describe GroupsController do
   describe 'GET new' do
 
     context 'with authenticated user' do
+      
       it 'renders the new template' do
         session[:user] = Fabricate(:user).id
         get :new
         expect(response).to render_template :new
       end
+      
     end
 
     context 'without authenticated user' do
-      it 'redirects to the login_path'
-      it 'displays a flash message'
+      
+      it 'redirects to the root_path' do
+        get :new
+        expect(response).to redirect_to root_path
+      end
+
+      it 'displays a flash message' do
+        get :new
+        expect(flash[:alert]).to eq('You must first sign in to access this page.')
+      end
     end
-
   end
-
 end
