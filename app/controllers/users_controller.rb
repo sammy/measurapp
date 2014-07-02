77e2ba968cea73810a2ede44ea1a598f6a6e3ff7
Sammy <flouts@gmail.com>
2014-07-02 11:18:15 +0200

    started implementing registration process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new
+    if @current_user
+      redirect_to home_path 
+      flash[:info] = 'You are already registered and signed in!'
+    else
+      @user = User.new
+    end
   end
 
   def create
+    binding.pry
+    redirect_to login_path
+    @user = User.new(user_params)
+    @user.save
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit!
+  end
 end
+
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,1 +1,54 @@
-require 'spec_helper'
+require 'rails_helper'
+
+describe UsersController do 
+
+  describe 'GET new' do
+    
+    context 'with authenticated user' do
+      let(:john) { Fabricate(:user) }
+      before { session[:user] = john.id }
+
+      it 'redirects to the home path' do
+        get :new
+        expect(response).to redirect_to home_path
+      end
+
+      it 'displays a flash message' do
+        get :new
+        expect(flash[:info]).to eq('You are already registered and signed in!')
+      end
+    end
+
+    context 'without authenticated user' do
+
+      it 'assigns the @user instance variable' do
+        get :new
+        expect(assigns(:user)).to be_kind_of(User)
+      end
+
+      it 'renders the new template' do
+        get :new
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe 'POST create' do
+    context 'with valid input' do
+      it 'creates a record in the database' do
+        post :create, user: { username: 'sammy', password: 'password', password_confirmation: 'password', email: 'flouts@gmail.com', first_name: 'ap', last_name: 'samat' }
+        expect(User.first.username).to eq('sammy')
+      end
+      it 'displays a flash message'
+      it 'redirects you to the login path' do
+        post :create, user: { username: 'sammy', password: 'password', password_confirmation: 'password', email: 'flouts@gmail.com', first_name: 'ap', last_name: 'samat' }
+        expect(response).to redirect_to login_path
+      end
+
+      it 'send you an registration email'
+    end
+    context 'with invalid input' do
+    end
+  end
+  
+end
