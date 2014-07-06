74ab53eb25f123f1c3a337584ccdd1adfea529fe
Sammy <flouts@gmail.com>
2014-07-06 12:22:43 +0200

    more tests on groups

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -72,37 +72,49 @@ describe GroupsController do
       let(:group)   { Fabricate.attributes_for(:group) }
       let(:item_1)  { Fabricate(:item) }
       let(:item_2)  { Fabricate(:item) }
-
+      
       before do
         session[:user] = mike.id
         post :create, group: group.merge(item_ids: [item_1.id, item_2.id])
       end
 
-      it 'redirects to the new_group_path' do
-        expect(response).to redirect_to new_group_path
+      context 'with valid input' do
+        
+
+        it 'redirects to the new_group_path' do
+          expect(response).to redirect_to new_group_path
+        end
+
+        
+        it 'creates a new group in the database' do
+          expect(Group.last.name).to eq(group[:name])
+          expect(Group.last.description).to eq(group[:description])
+        end
+
+        it 'assigns the new group to the current user' do
+          expect(Group.last.user_id).to eq(mike.id)
+        end
+
+        it 'associates selected items with the group' do
+          expect(Group.last.items).to eq([item_1, item_2])
+        end
+
+        it 'displays a flash message' do
+          expect(flash[:success]).to eq("Group #{group[:name]} was successfully created!")
+        end
       end
 
-      
-      it 'creates a new group in the database' do
-        expect(Group.last.name).to eq(group[:name])
-        expect(Group.last.description).to eq(group[:description])
-      end
+      context 'with invalid input' do
 
-      it 'assigns the new group to the current user' do
-        expect(Group.last.user_id).to eq(mike.id)
+        it 'does not save the group if name is not present' do
+          group[:name] = nil
+          expect(Group.count).to eq(0)
+        end
       end
-
-      it 'associates selected items with the group' do
-        expect(Group.last.items).to eq([item_1, item_2])
-      end
-
-      it 'displays a flash message' do
-        expect(flash[:success]).to eq("Group #{group[:name]} was successfully created!")
-      end
-
-      
     end
 
-    context 'with non authenticated user'
+    context 'with non authenticated user' do
+    end
   end
+
 end
